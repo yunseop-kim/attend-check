78319cff16b65050163cd26484c7d88fc10d9654
김윤섭 <yunsub2@gmail.com>
2019-12-05 13:21:23 +0900

    iframe 내에 찾아서 출석체크 클릭까지 하게 함

--- cypress/integration/attend.spec.js
+++ cypress/integration/attend.spec.js
@@ -6,22 +6,22 @@ import {
 } from '../constants/loginInfo';
 
 describe('출첵용', function() {
-  it('교보문고 로그인', function() {
-    cy.visit(
-      'http://www.kyobobook.co.kr/login/login.laf?Kc=GNHHNOlogin&orderClick=c03&retURL=http%3A//www.kyobobook.co.kr/event/dailyCheckSpci.laf%3ForderClick%3Dc1j'
-    );
+  // it('교보문고 로그인', function() {
+  //   cy.visit(
+  //     'http://www.kyobobook.co.kr/login/login.laf?Kc=GNHHNOlogin&orderClick=c03&retURL=http%3A//www.kyobobook.co.kr/event/dailyCheckSpci.laf%3ForderClick%3Dc1j'
+  //   );
 
-    // Get an input, type into it and verify that the value has been updated
-    cy.get('#memid')
-      .type(KYOBO_ID)
-      .should('have.value', KYOBO_ID);
+  //   // Get an input, type into it and verify that the value has been updated
+  //   cy.get('#memid')
+  //     .type(KYOBO_ID)
+  //     .should('have.value', KYOBO_ID);
 
-    cy.get('#pw')
-      .type(KYOBO_PW)
-      .should('have.value', KYOBO_PW);
+  //   cy.get('#pw')
+  //     .type(KYOBO_PW)
+  //     .should('have.value', KYOBO_PW);
 
-    cy.get('.btn_submit').click();
-  });
+  //   cy.get('.btn_submit').click();
+  // });
 
   it('11st 로그인 & 출첵', function() {
     cy.visit(
@@ -44,9 +44,16 @@ describe('출첵용', function() {
       .invoke('removeAttr', 'target')
       .click();
 
-    cy.get('[href="javascript:evtAttendJoin();"]').click();
-    // cy.get('[href="javascript:evtAttendFstJoin();"]').click();
-    // cy.get('[href="javascript:evtAttendScdJoin();"]').click();
-    // cy.get('[href="javascript:evtAttendThdJoin();"]').click();
+    // cy.get('.attend_chk').children().first().click();
+    cy.get('iframe[title="출석체크하기"]').then(function($iframe){
+      // query into the iframe
+      $iframe.find('[href="javascript:evtAttendJoin();"]').click();
+      $iframe.find('[href="javascript:evtAttendFstJoin();"]').click();
+      $iframe.find('[href="javascript:evtAttendScdJoin();"]').click();
+      $iframe.find('[href="javascript:evtAttendThdJoin();"]').click();
+      return null
+    })
+
+
   });
 });
